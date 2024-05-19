af57afe2802438038d33dca5c3d37e6277f44090
Saijal Shakya <saijalshakya@gmail.com>
2024-05-19 19:25:13 +1000

    Feat: imp comments added

--- app.py
+++ app.py
@@ -41,12 +41,17 @@ def load_user(user_id):
     return None
 
 
+# Function for registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
+        # Encrypt Password
         hashed_password = generate_password_hash(password)
+
+        # Inserting in mongodb
         users_collection.insert_one({"username": username, "password": hashed_password})
         flash('Registration successful. Please log in.', "success")
         return redirect(url_for('login'))
@@ -58,14 +63,20 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
+        # Search user with username
         user = users_collection.find_one({"username": username})
+
+        # Validate Password
         if user and check_password_hash(user['password'], password):
             login_user(User(str(user['_id']), user['username']))
+            flash("You are logged in successfully.", "success")
             return redirect(url_for('index'))
         flash('Invalid username or password.', "error")
     return render_template('login.html')
 
 
+# Logout user
 @app.route('/logout')
 @login_required
 def logout():
@@ -76,6 +87,7 @@ def logout():
 @app.route('/')
 @login_required
 def index():
+    # Get all data based on user id
     notes = notes_collection.find({"user_id": current_user.id})
     return render_template('Crud/notes.html', notes=notes)
 
@@ -86,6 +98,8 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+
+        # Get tags from the form and convert it to an array
         tags = request.form.get('tags', '').split(',')
         current_time = datetime.now()
         post = {
@@ -96,6 +110,8 @@ def create():
             'created_at': current_time,
             'updated_at': current_time
             }
+        
+        # insert into mongodb
         notes_collection.insert_one(post)
         flash("New note has been created successfully.", "success")
         return redirect(url_for('index'))
@@ -105,6 +121,7 @@ def create():
 @app.route('/view/<post_id>')
 @login_required
 def view_post(post_id):
+    # Get post from user id and object id
     notes = notes_collection.find_one({"_id": ObjectId(post_id), "user_id": current_user.id})
     if not notes:
         return redirect(url_for('index'))
@@ -114,14 +131,18 @@ def view_post(post_id):
 @app.route('/edit/<post_id>', methods=['GET', 'POST'])
 @login_required
 def edit_note(post_id):
+    # Find post first
     post = notes_collection.find_one({"_id": ObjectId(post_id), "user_id": current_user.id})
     if not post:
         return redirect(url_for('index'))
     if request.method == 'POST':
+        # Get all data from update form
         title = request.form['title']
         content = request.form['content']
         tags = request.form.get('tags', '').split(',')
         current_time = datetime.now()
+
+        # Update the note using object id
         notes_collection.update_one({'_id': ObjectId(post_id)}, {'$set': {'title': title, 'content': content, 'tags': tags, 'updated_at': current_time}})
         flash(f"{title} has been successfully updated.", "success")
         return redirect(url_for('index'))
@@ -129,6 +150,7 @@ def edit_note(post_id):
     return render_template('Crud/edit_note.html', post=post)
 
 
+# Deleting a note
 @app.route('/delete/<post_id>')
 @login_required
 def delete_note(post_id):
@@ -141,7 +163,10 @@ def delete_note(post_id):
 @login_required
 def search():
     if request.method == 'POST':
+        # get tags from search field
         search_tag = request.form['search']
+
+        # match tags using regular expression
         notes = notes_collection.find({'tags': {'$regex': search_tag, '$options': 'i'}})
         return render_template('search_results.html', notes=notes, search_name=request.form['search'])
     return redirect(url_for('index'))
